04f925a60afaf618ffc3185af31f5b04378f4644
Jaqueline <jaqcscarvalho@gmail.com>
2021-04-19 15:07:53 -0300

    Paciente

---
+++ PesquisaPCS/Models/Data/Paciente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PesquisaPCS.Models.Data
+{
+    public class Paciente
+    {
+        int idpaciente;
+    }
+}
